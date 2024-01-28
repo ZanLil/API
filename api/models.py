d58bf8e8ec0c2cf7c1033c6a7d9b5313599f727b
it-smt <smt_razrab@mail.ru>
2024-01-28 08:07:49 +0500

    changed and added new fields

--- api/models.py
+++ api/models.py
@@ -2,15 +2,25 @@ from django.db import models
 
 
 class PerevalAdded(models.Model):
+    """Модель с перевалами."""
+
+    class Status(models.TextChoices):
+        NEW = 'new', 'new'
+        PENDING = 'pending', 'pending'
+        ACCEPTED = 'accepted', 'accepted'
+        REJECTED = 'rejected', 'rejected'
+
     date_added = models.DateTimeField(auto_now_add=True)
     raw_data = models.JSONField()
     images = models.JSONField()
+    status = models.CharField(max_length=8, choices=Status.choices, default=Status.NEW)
 
     class Meta:
         db_table = 'pereval_added'
 
 
 class PerevalAreas(models.Model):
+    """Модель области."""
     parent = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE, related_name='areas')
     title = models.TextField()
 
@@ -19,6 +29,7 @@ class PerevalAreas(models.Model):
 
 
 class PerevalImages(models.Model):
+    """Модель с изображением."""
     date_added = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(upload_to='pereval_images/', blank=True, null=True)
 
@@ -27,6 +38,7 @@ class PerevalImages(models.Model):
 
 
 class SprActivitiesTypes(models.Model):
+    """Модель видов деятельности."""
     title = models.TextField()
 
     class Meta:
