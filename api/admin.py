0377c61448893173a5b057432dcca740915f0a58
it-smt <smt_razrab@mail.ru>
2024-01-28 08:22:30 +0500

    added models in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import PerevalAdded, PerevalAreas, PerevalImages, SprActivitiesTypes
+
+
+@admin.register(PerevalAdded)
+class PerevalAddedAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PerevalAreas)
+class PerevalAreasAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PerevalImages)
+class PerevalImagesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SprActivitiesTypes)
+class SprActivitiesTypesAdmin(admin.ModelAdmin):
+    pass
